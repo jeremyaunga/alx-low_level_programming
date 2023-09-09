23013d2f81fc67deedbb889d70a5863446b0b1e3
jeremyaunga <aungajeremiah1738@gmail.com>
2023-09-08 19:01:43 -0700

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 /**
-  * main - Prints two number combination
-  * 
-  * Return: - Always (Success)
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
   */
 int main(void)
 {
 	int c, i;
 
-	c = i = '0';
-
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(c);
-			putchar(i);
-
-			if ((c != '9') || (c == '9' && i != '9'))
+			if (c < i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
