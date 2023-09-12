2dcc2d70c996e0d658f6a2276e6fba1fadf9acc4
jeremyaunga <aungajeremiah1738@gmail.com>
2023-09-12 02:37:04 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
   * Return: Always 0
   */
 int main(void)
- { 
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,7 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-        _putchar('\n');
+	_putchar('\n');
 
 	return (0);
- }
+}
