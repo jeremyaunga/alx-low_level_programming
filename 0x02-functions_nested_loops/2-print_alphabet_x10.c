186e87a124d39aa633707af0d23afb09b200daf9
jeremyaunga <aungajeremiah1738@gmail.com>
2023-09-11 23:34:30 -0700

    alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+    int i, j;
+    char letter;
+
+    for (i = 0; i < 10; i++) 
+    {
+        letter = 'a';
+
+        for (j = 0; j < 26; j++) 
+        {
+            _putchar(letter); 
+ 	    letter++;         
+        }
+
+        _putchar('\n');
+    }
+}
+
