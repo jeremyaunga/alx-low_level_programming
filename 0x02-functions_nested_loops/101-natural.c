19bddc20ad0505e30eeed5f61d88f341e495aa30
jeremyaunga <aungajeremiah1738@gmail.com>
2023-09-12 03:36:50 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,17 +5,20 @@
   *
   * Return: Always (Success)
   */
-int main() {
-    int limit = 1024;
-    int sum = 0;
+int main(void)
+{
+	int i, z = 0;
 
-    for (int i = 3; i < limit; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
 
-    printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
-    return 0;
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
 }
-
