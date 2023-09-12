01ee6a16f5691c9cce7912b69685ce1464a1bdea
jeremyaunga <aungajeremiah1738@gmail.com>
2023-09-12 02:45:51 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,16 +6,12 @@
   *
   * Return: 1 for alphabetic character or 0 for anything else
   */
-
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
 
+	return (0);
+}
