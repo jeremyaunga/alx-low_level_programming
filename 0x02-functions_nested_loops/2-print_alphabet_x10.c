359459f5865baf64dd975ee79d1157b36ee8893c
jeremyaunga <aungajeremiah1738@gmail.com>
2023-09-12 02:30:12 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,23 +5,20 @@
   *
   * Return: void
   */
-
 void print_alphabet_x10(void)
 {
-    int i, j;
-    char letter;
+	char c;
+	int i = 0;
 
-    for (i = 0; i < 10; i++) 
-    {
-        letter = 'a';
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 
-        for (j = 0; j < 26; j++) 
-        {
-            _putchar(letter); 
- 	    letter++;         
-        }
+		_putchar('\n');
 
-        _putchar('\n');
-    }
+		i++;
+	}
 }
-
