e8c5f46643fa03d6aafa511d6339196d2fc74407
jeremyaunga <aungajeremiah1738@gmail.com>
2023-10-05 06:13:48 -0700

    checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,3 +8,15 @@
  * if malloc fails, status value is equal to 98.
  */
 void *malloc_checked(unsigned int b)
+{
+/* using void for multiple vars being of different types*/
+void *alloc;
+
+alloc = malloc(b);
+
+if (alloc == NULL)
+{
+exit(98);
+}
+return (alloc);
+}
